965749e486278a5bd53892695f7ae51fb9981b6f
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-06-06 06:48:45 +0200

    meca rdv OK

--- views/pages/visiteur/mecanique.view.php
+++ views/pages/visiteur/mecanique.view.php
@@ -1,10 +1,58 @@
-<div class="pageMecanique loc-location">
+<div class="pageMecanique pageSorties">
 
     <h1>L'entretien de vos montures</h1>
-    
-    <img src="public/assets/images/mecanique/atelier.jpg" alt="atelier vélo">
-   
-   
+
+    <div class="enteteMeca">
+        <img src="public/assets/images/mecanique/atelier.jpg" alt="atelier vélo">
+
+        <h2>Du réglage le plus simple à la révision complète de votre vélo (électrique ou pas), laissez nous faire. </h2>
+        <h2>Votre vélo ne doit être là que pour vous laisser profiter de nos plus beaux chemins !</h2>
+    </div>
+
+    <div class="sorties1">
+        <img src="public/assets/images/mecanique/meca1.jpeg" alt="">
+        <h3 class="h3Sorties">Afin de vous garantir les prix les plus justes, nous préférons voir votre vélo pour apprécier de cec qu'il y a affaire dessus. <br><br> Votre porte-monnaie n'aime pas les surprise ? Nous n'aimons pas lui en faire ! 👛</h3>
+
+    </div>
+    <div class="sorties1 sorties2">
+        <h3 class="h3Sorties">Que nous ayons votre pièce, qu'il faille la commander ou que vous la rameniez de chez vous, nous réparons et entretenons votre vélos. <br><br> Ne pensez qu'à votre prochaine sortie ! 😉</h3>
+        <img src="public/assets/images/mecanique/meca2.jpg" alt="">
+
+    </div>
+    <div class="sorties1">
+        <img src="public/assets/images/mecanique/meca3.jpg" alt="">
+        <h3 class="h3Sorties">Entretien de votre fourche, dévoilage de vos roues (oui, on a quelques cailloux dans nos chemin 😅)...<br><br>Confiez nous l'entretien de votre compagnon de sortie. Nous gérons de A à Z !</h3>
+
+    </div>
+
+    <div class="sorties1 sorties2 lastMeca">
+        <h3 class="h3Sorties">Que ce soit le dernier vélo carbone électrique our le vieux vélo de papy qui traine dans son garage depuis 38 ans... <br><br> Ils sont tous les bienvenus à Velo Expresso!💕</h3>
+        <img src="public/assets/images/mecanique/meca4.jpg" alt="">
+
+    </div>
+    <!-- <h2>Les groupes possibles</h2>
+    <div class="groupesSorties">
+        <div class="groupeSorties">
+            <h2 class="nomGroupe">Les Cosi :</h2><br><br><p>Pour une balade conviviale et découvrir ou redécouvrir nos plus beaux parcours avec comme objectif : le plaisir de rouler ensemble.<br><br>Groupe accessible à toutes et tous évidemment ! Vous découvrez ? Laissez vous porter par le groupe !</p>
+        </div>
+
+        <div class="groupeSorties">
+
+            <h2 class="nomGroupe">Les Ristretto :</h2><br><br><p>Un objectif simple => les crampes ! <br><br>Oui, là on cherche à se rentrer dedans. Le goût de l'effort et du challenge physique. Mais toujours avec le sourire et la convivialité !</p>
+        </div>
+
+        <div class="groupeSorties">
+
+            <h2 class="nomGroupe">Les Cafés Frappés :</h2><br><br><p> "il est où le camion pour nous monter ? nous, on veut faire des descentes !!!".<br><br> Bon vous l'avez compris, ici on priorise le débattement, les descentes, les sauts...</p>
+
+        </div>
+    </div>
+    <h3 class="conclusionSortie">Les répartitions se font naturellemnt pour que chacun passe le meilleur moment possible !😜</h3> -->
+
+
+
+</div>
+
 
 
 </div>
